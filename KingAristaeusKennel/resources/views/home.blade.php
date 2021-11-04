adac009290f3ab3822d39c3707385a5eadc848d3
Ghelonico Maligaya <ghelonicomaligaya111@gmail.com>
2021-11-04 18:05:42 +0800

    first commit

---
+++ KingAristaeusKennel/resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>King Aristaeus</title>
+    <link rel="stylesheet" href="/css/home/home.css">
+</head>
+<body>
+    <div class = 'logo'>
+        <img src="{{url('/img/Logo_w_crown.png')}}" alt="Image"/>
+        
+        <div class='buttons-layer'>
+            <button type="submit">Login</button>
+            <button type="submit">Register</button>
+        </div>
+    </div>
+</body>
+</html>
