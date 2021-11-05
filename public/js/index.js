43b6ca486aa2f4e54108f65ca3d66477a2081974
Ghelonico Maligaya <ghelonicomaligaya111@gmail.com>
2021-11-05 16:36:53 +0800

    added navbar

---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+// var toggle = true;
+
+// function toggleNavBar() {
+//     if (toggle){
+//         document.getElementById("navBar").style.height = "7.5vh";
+
+//         this.toggle = false;
+//     } else {
+//         document.getElementById("navBar").style.height = "5vh";
+//         this.toggle = true;
+//     }
+// }
---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>King Aristaeus</title>
+    <link rel="stylesheet" href="/css/index/index.css">
+</head>
+<body>
+    <div class='nav-bar' id='navBar'>
+        <div class='logo'>
+            <a href="/"><img src="/img/husky_logo_noBG.png" alt="husky_logo"> </a>
+        </div>
+        <div class='nav-buttons'>
+            <a href="/">Home</a>
+            <a href="/">Kennel Family Tree</a>
+            <a href="/">Location</a>
+            <a href="/">Contact us</a>
+            <a href="/">PCCI Certificate</a>
+        </div>
+    </div>
+    {{-- <script src="{{ url('js/index.js') }}"></script> --}}
+</body>
+</html>
