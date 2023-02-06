461b94fab679efde3657d8059ec30ecd83dfed4c
HAPPY <ghelonicomaligaya111@gmail.com>
2023-02-06 22:49:23 +0800

    fixed login design

--- routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PagesController;
-
-Route::get('/', [PagesController::class, 'index']);
-Route::get('/location', [PagesController::class, 'location']);
-Route::get('/contact', [PagesController::class, 'contact']);
